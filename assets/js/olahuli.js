232caff40867d3affe5b565b3c7dda7ce90c4801
Josiah De La Torre <jodelat@yahoo.com>
2017-08-12 19:55:20 -0700

    getty api update

--- assets/js/olahuli.js
+++ assets/js/olahuli.js
@@ -1,16 +1,16 @@
 
 $(document).on("click", "#button", function(){
   var input = $("#textInput").val().trim();
-
+  var gettyApi = "9pmqn85smrhhhx3wq5zjhgk7";
 
   var firstURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q="+ input +"&type=video&key=AIzaSyB4_u1AG7P6ykJ68p080pjfIpjjZk7SJP0";
-
+  var secondURL = "https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=" + input;
   $.ajax({
     url: firstURL,
     method: "GET"
   }).done(function(response){
 
-    var newDiv = $("<div id ='youtube'>";)
+    var newDiv = $("<div id ='youtube'>");
     // for(i = 0; i < response.items.length; i++){
     //   var videos = response.items[i].id.videoId;
     //   var vidTag = $("<iframe width='200' height='150' class='videos'>
@@ -20,6 +20,24 @@ $(document).on("click", "#button", function(){
     //$("#htmlTag").append(newDiv);
     console.log(response.items[i].id.videoId);
 
+  });
+
+  $.ajax({
+    url: secondURL,
+    method: "GET",
+    beforeSend: function(request){
+      request.setRequestHeader("Api-Key", gettyApi);
+    }
+
+  }).done(function(response){
+    var newDiv = $("<div id='getty'>");
+    //for(i = 0; i < 11; i++){
+      //var image = response.images[i].display_sizes[0].uri;
+      //var imageTag = $("<img>").attr("src", image);
+      //newDiv.append(imageTag);
+    //}
+    //$("#htmlTag").append(newDiv);
+    console.log(response);
   });
 
 
