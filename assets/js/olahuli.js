b647d88d60d371abb5aaec845610de0388478737
Josiah De La Torre <jodelat@yahoo.com>
2017-08-14 18:48:12 -0700

    added wiki api

--- assets/js/olahuli.js
+++ assets/js/olahuli.js
@@ -6,6 +6,29 @@ $(document).on("click", ".add-search-data", function(){
   console.log(input);
   var firstURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=8&q="+ input +"&type=video&key=AIzaSyB4_u1AG7P6ykJ68p080pjfIpjjZk7SJP0";
   var secondURL = "https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=most_popular&phrase=" + input;
+  var thirdURL="https://en.wikipedia.org/w/api.php?action=parse&origin=*&prop=text&format=json&redirects=1&page=" + input;
+  $.ajax({
+    url: thirdURL,
+    method: "GET"
+  }).done(function(response){
+     console.log(response);
+     console.log(thirdURL);
+     $(".wiki-view").empty();
+
+   var newDiv = $("<div class ='col-xs-12 search-div'>");
+
+
+     var title = response.parse.title;
+      var content = response.parse.text["*"];
+
+
+     newDiv.append(title);
+      newDiv.append(content);
+
+   $(".wiki-view").append(newDiv);
+    console.log(response);
+  });
+
   $.ajax({
     url: firstURL,
     method: "GET"
