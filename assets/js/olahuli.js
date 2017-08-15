008773995fe819f8e39f15c3293ce2d7c4c9c044
Josiah De La Torre <jodelat@yahoo.com>
2017-08-14 19:22:12 -0700

    fixed indentations to clean code up

--- assets/js/olahuli.js
+++ assets/js/olahuli.js
@@ -3,10 +3,10 @@ $(document).on("click", ".add-search-data", function(){
 
   var input = $(".wiki-search-input").val().trim();
   var gettyApi = "9pmqn85smrhhhx3wq5zjhgk7";
-  console.log(input);
   var firstURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=8&q="+ input +"&type=video&key=AIzaSyB4_u1AG7P6ykJ68p080pjfIpjjZk7SJP0";
   var secondURL = "https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=most_popular&phrase=" + input;
   var thirdURL="https://en.wikipedia.org/w/api.php?action=parse&origin=*&prop=text&format=json&redirects=1&page=" + input;
+
   $.ajax({
     url: thirdURL,
     method: "GET"
@@ -15,25 +15,24 @@ $(document).on("click", ".add-search-data", function(){
      console.log(thirdURL);
      $(".wiki-view").empty();
 
-   var newDiv = $("<div class ='col-xs-12 search-div'>");
+     var newDiv = $("<div class ='col-xs-12 search-div'>");
 
 
      var title = response.parse.title;
-      var content = response.parse.text["*"];
+     var content = response.parse.text["*"];
 
 
      newDiv.append(title);
-      newDiv.append(content);
+     newDiv.append(content);
 
-   $(".wiki-view").append(newDiv);
-    console.log(response);
+     $(".wiki-view").append(newDiv);
+     console.log(response);
   });
 
   $.ajax({
     url: firstURL,
     method: "GET"
   }).done(function(response){
-
     var newDiv = $("<div class='row' id ='youtube'>");
     for(i = 0; i < response.items.length; i++){
       var videos = response.items[i].id.videoId;
@@ -52,7 +51,6 @@ $(document).on("click", ".add-search-data", function(){
     }
 
   }).done(function(response){
-
     var newDiv = $("<div class='row' id='getty'>");
     for(i = 0; i < 8; i++){
       var image = response.images[i].display_sizes[0].uri;
